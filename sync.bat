7497bbfa4b3e41b7828221917e80d119ff2db4b7
Jiang Ben <47379449@qq.com>
2025-05-28 16:46:08 +0800

    直玩推送脚本

--- sync.bat
+++ sync.bat
@@ -6,7 +6,9 @@ set GOARCH=amd64
 go build -o nakama
 #scp -P 22  nakama   root@192.168.102.223:/root/hjdt/bin
 
-scp -P 31222  nakama   root@118.145.200.91:/root/wx-xjsm/bin
+#星际使命直玩
+scp -P 31222  nakama   root@118.145.206.63:/root/dykpbl/bin
+# scp -P 31222  nakama   root@118.145.200.91:/root/wx-xjsm/bin
 
 rem scp -P 31222  nakama   root@47.122.45.71:/root/nakama/bin
 rem  scp -P 31222  nakama   root@8.138.94.100:/root/nakama/bin
